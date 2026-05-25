921b158a8d56feeeeda0f4f4830e55839c685cf5
Sonichac32 <manvithreddya2@gmail.com>
2026-05-24 21:53:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ async function sendMessage() {
 
   try {
 
-    const response = await fetch("http://localhost:3000/chat", {
+    const response = await fetch("/chat", {
 
       method: "POST",
 
